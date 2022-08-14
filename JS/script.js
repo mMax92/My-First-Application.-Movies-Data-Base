e03d52b561cfb6a3e50232cdbe9f771f7f9f113b
MMax <mr.moshnyatskiy@gmail.com>
2022-08-14 17:48:17 +0300

    Commit 3.0. Modified with loop while

--- JS/script.js
+++ JS/script.js
@@ -10,7 +10,10 @@ const personalMovieDB = {
     private: false
 };
 
-for (let i = 0; i < 2; i++) {
+let i = 0;
+
+while (i < 2) {
+    i++;
     const a = prompt("Один из последних просмотренных фильмов", ""),
           b = prompt("На сколько оцените его?", "");
     if (a != null && b != null && a != "" && b != "" && a.length < 50) {
