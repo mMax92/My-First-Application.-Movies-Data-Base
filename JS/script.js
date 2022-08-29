544527338d03a2e91c29363382531c2cc811773b
MMax <mr.moshnyatskiy@gmail.com>
2022-08-29 17:55:37 +0300

    Commit 5.2. Added .trim()

--- JS/script.js
+++ JS/script.js
@@ -20,21 +20,16 @@ const personalMovieDB = {
     privat: false
 };
 
-let i = 0;
-
-function rememberMyFilms() {
-    do  {
-        i++;
-        const a = prompt("Один из последних просмотренных фильмов", ""),
-              b = prompt("На сколько оцените его?", "");
-        if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log("done");
-        } else {
-            console.log("error!!");
-            i--;
-        }
-    } while (i < 2);
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов", ""),
+          b = prompt("На сколько оцените его?", "");
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+    } else {
+        console.log("error!!");
+        i--;
+    }
 }
 
 rememberMyFilms();
